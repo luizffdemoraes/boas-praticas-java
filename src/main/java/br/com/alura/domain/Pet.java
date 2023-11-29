dfbda0cca6ac39a8f5c3cb57e2c127654ba3050f
Luiz Felipe <lffm1994@gmail.com>
2023-11-29 01:12:18 -0300

    Ajustando o método listarPets

--- src/main/java/br/com/alura/domain/Pet.java
+++ src/main/java/br/com/alura/domain/Pet.java
@@ -1,6 +1,7 @@
 package br.com.alura.domain;
 
 public class Pet {
+    private Long id;
     private String tipo;
     private String nome;
     private String raca;
@@ -15,5 +16,29 @@ public class Pet {
         this.idade = idade;
         this.cor = cor;
         this.peso = peso;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Float getPeso() {
+        return peso;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public int getIdade() {
+        return idade;
     }
 }
